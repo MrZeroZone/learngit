0a5f601cf5c4cb5a9c4d9fa984182e0c9c4db9ac
Coder <528805963@qq.com>
2017-02-25 20:02:28 +0800

    modify file

--- python/demo.py
+++ python/demo.py
@@ -259,3 +259,151 @@
 #     print "%d 是一个回文数！" % a
 # else:
 #     print "%d 不是一个回文数！" % a
+
+# 31：请输入星期几的第一个字母来判断一下是星期几，如果第一个字母一样，则继续判断第二个。
+# letter = raw_input("please input:")
+# # while letter != 'Y':
+# if letter == 'S':
+#     print('please input second letter:')
+#     letter = raw_input("please input:")
+#     if letter == 'a':
+#         print('Saturday')
+#     elif letter == 'u':
+#         print('Sunday')
+#     else:
+#         print('data error')
+# elif letter == 'F':
+#     print('Friday')
+# elif letter == 'M':
+#     print('Monday')
+# elif letter == 'T':
+#     print('please input second letter')
+#     letter = raw_input("please input:")
+#     if letter == 'u':
+#         print('Tuesday')
+#     elif letter == 'h':
+#         print('Thursday')
+#     else:
+#         print('data error')
+# elif letter == 'W':
+#     print('Wendesday')
+# else:
+#     print('data error')
+
+# 32：按相反的顺序输出列表的值
+# a = ['one', 'two', 'three']
+# for i in a[::-1]:
+#     print i
+
+# 33：按逗号分隔列表
+# L = [1, 2, 3, 4, 5]
+# s1 = ','.join(str(n) for n in L)
+# print s1
+
+# 34:练习函数的调用
+# def hello_world():
+#     print 'hello world'
+
+
+# def three_hellos():
+#     for i in range(3):
+#         hello_world()
+
+# if __name__ == '__main__':
+#     three_hellos()
+
+# 35：文本颜色设置
+# class bcolors:
+#     HEADER = '\033[95m'
+#     OKBLUE = '\033[94m'
+#     OKGREEN = '\033[92m'
+#     WARNING = '\033[93m'
+#     FALE = '\033[91m'
+#     ENDC = '\033[0m'
+#     BOLD = '\033[1m'
+#     UNDERLINE = '033[4m'
+# print bcolors.WARNING+"警告的颜色字体？"+bcolors.ENDC
+
+# 36：求100之内的素数
+
+# 输出指定范围内的素数
+
+# lower = int(input("输入区间最小值："))
+# upper = int(input("输入区间最大值："))
+# for num in range(lower, upper+1):
+#     # 素数大于1
+#     if num > 1:
+#         for i in range(2, num):
+#             if (num % i) == 0:
+#                 break
+#         else:
+#             print(num)
+
+# 37：对10个数进行排序
+# if __name__ == '__main__':
+#     N = 10
+#     # input data
+#     print 'please input ten num:\n'
+#     l = []
+#     for i in range(N):
+#         l.append(int(raw_input('input a number:\n')))
+#     print
+#     for i in range(N):
+#         print l[i]
+#     print
+
+#     # sort ten num
+#     for i in range(N-1):
+#         min = i
+#         for j in range(i+1, N):
+#             if l[min] > l[j]:
+#                 min = j
+#         l[i], l[min] = l[min], l[i]
+#     print 'after sorted'
+#     for i in range(N):
+#         print l[i]
+
+# 38：求一个3×3矩阵对角线元素之和
+# if __name__ == '__main__':
+#     a = []
+#     sum = 0.0
+#     for i in range(3):
+#         a.append([])
+#         for j in range(3):
+#             a[i].append(float(raw_input("input num:\n")))
+#     for i in range(3):
+#         sum += a[i][i]
+#     print sum
+
+# 39：有一个已经排好序的数组。现输入一个数，要求按原来的规律将它插入数组中。
+# if __name__ == '__main__':
+#     # 方法一
+#     a = [1, 4, 6, 9, 13, 16, 19, 28, 40, 100, 0]
+#     print 'original list is:'
+#     for i in range(len(a)):
+#         print a[i]
+#     number = int(raw_input("insert a new number:\n"))
+#     end = a[9]
+#     if number > end:
+#         a[10] = number
+#     else:
+#         for i in range(10):
+#             if a[i] > number:
+#                 temp1 = a[i]
+#                 a[i] = number
+#                 for j in range(i+1, 11):
+#                     temp2 = a[j]
+#                     a[j] = temp1
+#                     temp1 = temp2
+#                 break
+#     for i in range(11):
+#         print a[i]
+
+# 40：将一个数组逆序输出
+# if __name__ == '__main__':
+#     a = [9, 6, 5, 4, 1]
+#     N = len(a)
+#     print a
+#     for i in range(len(a)/2):
+#         a[i], a[N-i-1] = a[N-i-1], a[i]
+#     print a
